8457bcfee9e45580ff63f96cce824296cf005f74
jvdbogae <joachim@croslang.com>
2019-09-20 10:46:26 +0200

    Added dir

--- joachim/hello.py
+++ joachim/hello.py
@@ -1,3 +1,15 @@
 import logging
 
+def say_hello():
+  logging.basicConfig(level=logging.DEBUG)
+  logging.info("Hello world")
 
+say_hello()
+
+# Single line comment
+'''
+In java we have /**
+                **/
+'''
+if __name__=="__main__":
+  say_hello()
