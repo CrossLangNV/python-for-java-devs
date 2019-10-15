343fc8452a9bd8fd0940b166e9cda5c989d33892
anna.bardadym <anna.bardadym@crosslang.com>
2019-10-15 11:35:01 +0200

    improved the showcase

--- anna/basic-oo-methods-showcase/showcase.py
+++ anna/basic-oo-methods-showcase/showcase.py
@@ -1,6 +1,9 @@
 class C(object):
-    def __init__(self):
-        self.__x = None
+
+    # Voorbeelden van verschillende mogelijke constructors:
+
+    #def __init__(self):
+    #    self.__x = None
 
     #def __init__(self, x):
     #    self.__x = x
@@ -8,29 +11,41 @@ class C(object):
     #def __init__(self, x=None):
     #    self.__x = x
 
-    #def __init__(self, x="something"):
-    #    self.__x = x
+    def __init__(self, x="something"):
+        self.__x = x
 
-    #@property
-    #def x(self):
-    #    print("getter of x called")
-    #    return self.__x
+    # Getters en setters:
 
-    #@x.setter
-    #def x(self, value):
-    #    print("setter of x called")
-    #    self.__x = value
+    @property
+    def x(self):
+        print("getter of x called")
+        return self.__x
 
-    #def __repr__(self):
-    #    return "C x=" + self.__x
+    # een getter is ook op de volgende manier mogelijk:
+    def get_x(self):
+        return self.__x
 
-    #def __str__(self):
-    #    return "I am an object of class C. The value of my x property is " + self.__x
+    @x.setter
+    def x(self, value):
+        print("setter of x called")
+        self.__x = value
+
+    # Methodes om classeobject als string weer te geven:
+
+    def __repr__(self):
+        return "C __x=" + self.__x
+
+    def __str__(self):
+        return "I am an object of class C. The value of my x property is " + self.__x
 
 c = C()
+c2 = C("bla")
 
-#c.x = 'foo'  # setter called
-#foo = c.x    # getter called
+c.x = 'foo'  # setter called
+bar = c.x    # getter called
 
-#c.__repr__()
-#c.__str__()
+print(c.__repr__())
+print(c.__str__())
+
+print(c)
+
