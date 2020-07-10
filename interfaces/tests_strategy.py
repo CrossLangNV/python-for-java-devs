c9c9b27d759980c6ad2fff9c79ca71f533d29298
lameeus <laurens.meeus@ugent.be>
2020-07-10 11:08:44 +0200

    solution laurens. with class type checking

--- interfaces/tests_strategy.py
+++ interfaces/tests_strategy.py
@@ -1,28 +1,27 @@
 from unittest import TestCase
 
-from strategy import Order, FedExStrategy, UPSStrategy, PostalStrategy, ShippingCost
+from shippingstrategy import Order, FedExStrategy, UPSStrategy, PostalStrategy, ShippingCost
 
 
 class TestsStrategy(TestCase):
 
     def test_shipping_costs(self):
         # test FedEx shipping
-        order = Order()
         strategy = FedExStrategy()
-        shipping_cost = ShippingCost(strategy)
-        cost = shipping_cost.calculate(order)
-        assert cost == 3.0
+        assert test_shipping_costs_shared(strategy) == 3.0
 
         # test UPS shipping
-        order = Order()
         strategy = UPSStrategy()
-        shipping_cost = ShippingCost(strategy)
-        cost = shipping_cost.calculate(order)
-        assert cost == 4.0
+        assert test_shipping_costs_shared(strategy) == 4.0
 
         # test Postal Service shipping
-        order = Order()
         strategy = PostalStrategy()
-        shipping_cost = ShippingCost(strategy)
-        cost = shipping_cost.calculate(order)
-        assert cost == 5.0
+        assert test_shipping_costs_shared(strategy) == 5.0
+
+
+def test_shipping_costs_shared(strategy):
+    order = Order()
+    shipping_cost = ShippingCost(strategy)
+    cost = shipping_cost.calculate(order)
+
+    return cost
