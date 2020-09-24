f73c4b185059b27b6ad2a743f27e71cdfe13baee
neuro <laurensmeeus93@gmail.com>
2020-09-24 16:37:35 +0200

    Howto types of docstring formats in python.

---
+++ documentation/docstring_formats.py
@@ -0,0 +1,70 @@
+"""
+Automatically generated docstring, with different docstring formats.
+
+For Pycharm:
+File -> Settings -> Tools -> Python integrated tools -> Docstrings -> Docstring format
+"""
+
+
+# Plain
+def plain(word: str,
+          number: int) -> list:
+    """
+    This is for plain
+
+    """
+
+
+def epytext(word: str,
+            number: int) -> list:
+    """
+    This is for epytext
+
+    @param word: a word
+    @param number: a number
+    @return:
+    """
+
+
+def restructeredtext(word: str,
+                     number: int) -> list:
+    """
+    This is for restructeredtext
+
+    :param word: a word
+    :param number: a number
+    :return:
+    """
+
+
+def numpy(word: str,
+          number: int) -> list:
+    """
+    This is for numpy
+
+    Parameters
+    ----------
+    word
+        a word
+    number
+        a number
+
+    Returns
+    -------
+
+    """
+
+
+# Google
+def google(word: str,
+           number: int) -> list:
+    """
+    This is for google
+
+    Args:
+        word: a word
+        number: a number
+
+    Returns:
+
+    """
